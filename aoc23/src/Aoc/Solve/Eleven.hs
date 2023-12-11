4991755447bfaa7a18528b2a8bfcf849a3238a65
handdara <handdara.core@gmail.com>
2023-12-11 15:33:49 -0500

    day 11 solution

--- aoc23/src/Aoc/Solve/Eleven.hs
+++ aoc23/src/Aoc/Solve/Eleven.hs
@@ -1,21 +1,9 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
-
 module Aoc.Solve.Eleven
   ( solveDay11,
-    -- dbg
-    Input,
-    Space,
-    Direction (..),
-    AxisVal,
-    Coord,
-    Line (..),
-    Galaxies,
-    GalaxyMap (..),
-    solutionPart1,
-    solutionPart2,
   )
 where
-import Debug.Trace (trace)
+
+import qualified Data.List as L
 
 -- * Types
 
@@ -25,9 +13,9 @@ data Space = Empty | Galaxy
   deriving (Show, Eq)
 
 data Direction = PlusX | PlusY
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
-type AxisVal = Int
+type AxisVal = Integer
 
 type Coord = (AxisVal, AxisVal)
 
@@ -36,9 +24,31 @@ type Vector = (AxisVal, AxisVal)
 type Range = (AxisVal, AxisVal)
 
 data Line = Line Coord Direction
-  deriving (Show)
+  deriving (Show, Eq)
+
+instance Ord Line where
+  (Line cl dl) `compare` (Line cr dr) =
+    case compare dl dr of
+      EQ -> compare cl cr
+      res -> res
 
 type Galaxies = [Coord]
+
+data Segment = Segment Coord Coord
+  deriving (Show)
+
+instance Eq Segment where
+  (Segment lcl lcr) == (Segment rcl rcr) =
+    min lcl lcr == min rcl rcr
+      && max lcl lcr == max rcl rcr
+
+instance Ord Segment where
+  compare sl sr = case compare (getMin sl) (getMin sr) of
+    EQ -> compare (getMax sl) (getMax sr)
+    res -> res
+    where
+      getMin (Segment cl cr) = min cl cr
+      getMax (Segment cl cr) = max cl cr
 
 data GalaxyMap = GalaxyMap Range Range Galaxies
   deriving (Show)
@@ -54,54 +64,75 @@ parseLine y cs = foldl foldCharXCoord [] char'x'zip
       _ -> acc
 
 prepareInput :: Input -> GalaxyMap
-prepareInput i = GalaxyMap (0,x'max) (0,y'max) galaxies
+prepareInput i = GalaxyMap (0, fromIntegral x'max) (0, fromIntegral y'max) galaxies
   where
     x'max = (maximum . map length $ i) - 1
     y'max = length i - 1
-    line'y'zip = zip (reverse i) [0..]
-    foldLineYCoord acc (l',y') = acc <> parseLine y' l'
+    line'y'zip = zip (reverse i) [0 ..]
+    foldLineYCoord acc (l', y') = acc <> parseLine y' l'
     galaxies = foldl foldLineYCoord [] line'y'zip
+
+-- renderGM :: GalaxyMap -> Input
+-- renderGM (GalaxyMap (xl, xh) (yl, yh) gs) =
+--   reverse
+--     [ [mkCh (x, y) | x <- [xl .. xh]]
+--       | y <- [yl .. yh]
+--     ]
+--   where
+--     mkCh c = if c `elem` gs then '#' else '.'
 
 -- * Solutions
 
 isEmpty :: Line -> Galaxies -> Bool
-isEmpty (Line (_,y) PlusX) = not . any (\(_,y'g) -> y'g == y)
-isEmpty (Line (x,_) PlusY) = not . any (\(x'g,_) -> x'g == x)
+isEmpty (Line (_, y) PlusX) = not . any (\(_, y'g) -> y'g == y)
+isEmpty (Line (x, _) PlusY) = not . any (\(x'g, _) -> x'g == x)
 
 fdEmptyYLines :: GalaxyMap -> [Line]
-fdEmptyYLines (GalaxyMap (x'min,x'max) _ gs) = foldl accumEmptyXLines [] [x'min..x'max]
+fdEmptyYLines (GalaxyMap (x'min, x'max) _ gs) = foldl accumEmptyXLines [] [x'min .. x'max]
   where
     accumEmptyXLines ls x =
-      let l = Line (x,0) PlusY
-      in if isEmpty l gs
-        then l:ls
-        else ls
+      let l = Line (x, 0) PlusY
+       in if isEmpty l gs
+            then l : ls
+            else ls
 
 fdEmptyXLines :: GalaxyMap -> [Line]
-fdEmptyXLines (GalaxyMap _ (y'min,y'max) gs) = foldl accumEmptyYLines [] [y'min..y'max]
+fdEmptyXLines (GalaxyMap _ (y'min, y'max) gs) = foldl accumEmptyYLines [] [y'min .. y'max]
   where
     accumEmptyYLines ls y =
-      let l = Line (0,y) PlusX
-      in if isEmpty l gs
-        then l:ls
-        else ls
+      let l = Line (0, y) PlusX
+       in if isEmpty l gs
+            then l : ls
+            else ls
 
 fdEmptyLines :: GalaxyMap -> [Line]
-fdEmptyLines = (<>) <$> fdEmptyXLines <*> fdEmptyYLines
+fdEmptyLines =
+    L.sort
+    . ((<>) <$> fdEmptyXLines <*> fdEmptyYLines)
 
-expandEmptyLines :: GalaxyMap -> [Line] -> GalaxyMap
-expandEmptyLines = undefined
+expandLine :: AxisVal -> Line -> GalaxyMap -> GalaxyMap
+expandLine n (Line (x, y) d) (GalaxyMap xr@(x'min, x'max) yr@(y'min, y'max) gs) =
+  case d of
+    PlusX -> GalaxyMap xr (y'min, y'max + n - 1) (gs'idX <> gs'incX)
+    PlusY -> GalaxyMap (x'min, x'max + n - 1) yr (gs'idY <> gs'incY)
+  where
+    gs'idX = [c | c@(_, y'g) <- gs, y'g <= y]
+    gs'incX = [(x'g, y'g + n - 1) | (x'g, y'g) <- gs, y < y'g]
+    gs'idY = [c | c@(x'g, _) <- gs, x'g <= x]
+    gs'incY = [(x'g + n - 1, y'g) | (x'g, y'g) <- gs, x < x'g]
 
-expandGalaxy :: GalaxyMap -> GalaxyMap
-expandGalaxy = expandEmptyLines <*> fdEmptyLines
+-- | The right fold is important because the lines farther from the origin
+-- must be expanded first. Doing it in this order ensures that lines do not
+-- need to shift other lines when expanding
+expandEmptyLines :: AxisVal -> GalaxyMap -> [Line] -> GalaxyMap
+expandEmptyLines n = foldr (expandLine n)
 
-getGalaxyPairs :: GalaxyMap -> [(Coord, Coord)]
-getGalaxyPairs (GalaxyMap _ _ gs) = do
-  l <- gs
-  r <- gs
-  if l /= r
-    then return (l,r)
-    else []
+expandGalaxy :: AxisVal -> GalaxyMap -> GalaxyMap
+expandGalaxy n = expandEmptyLines n <*> fdEmptyLines
+
+getGalaxyPairs :: GalaxyMap -> [Segment]
+getGalaxyPairs (GalaxyMap _ _ gs) =
+  [Segment l r | l <- gs, r <- gs, l < r]
 
 diff :: Coord -> Coord -> Vector
 diff (xl, yl) (xr, yr) = (xr - xl, yr - yl)
@@ -110,22 +141,34 @@ normL1 :: Vector -> AxisVal
 normL1 (x, y) = abs x + abs y
 
 sumDistances :: GalaxyMap -> AxisVal
-sumDistances = sum . map (normL1 . uncurry diff) . getGalaxyPairs
+sumDistances =
+  sum
+    . map (normL1 . (\(Segment l r) -> diff l r))
+    . getGalaxyPairs
+
+expansionFactor'part1 :: AxisVal
+expansionFactor'part1 = 2
 
 solutionPart1 :: Input -> AxisVal
-solutionPart1 = sumDistances . expandGalaxy . prepareInput
+solutionPart1 =
+  sumDistances
+    . expandGalaxy expansionFactor'part1
+    . prepareInput
 
-solutionPart2 :: Input -> String
-solutionPart2 = const ("In Progress" :: String)
+expansionFactor'part2 :: AxisVal
+expansionFactor'part2 = 10 ^ (6 :: Integer)
+
+solutionPart2 :: Input -> AxisVal
+solutionPart2 =
+  sumDistances
+    . expandGalaxy expansionFactor'part2
+    . prepareInput
 
 solveDay11 :: FilePath -> IO ()
 solveDay11 input'path = do
   input'string <- readFile input'path
 
   let input'lines = lines input'string
-
-  putStrLn "Input: "
-  mapM_ putStrLn input'lines
 
   putStrLn $ "Solution to part 1: " <> show (solutionPart1 input'lines)
 
