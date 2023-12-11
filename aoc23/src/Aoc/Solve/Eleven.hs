e52747f76826c03b77d3443a0989318bbd264821
handdara <handdara.core@gmail.com>
2023-12-11 01:21:20 -0500

    day 11 input parsing & find empty lines

--- aoc23/src/Aoc/Solve/Eleven.hs
+++ aoc23/src/Aoc/Solve/Eleven.hs
@@ -4,18 +4,100 @@ module Aoc.Solve.Eleven
   ( solveDay11,
     -- dbg
     Input,
+    Space,
+    Direction (..),
+    AxisVal,
+    Coord,
+    Line (..),
+    Galaxies,
+    GalaxyMap (..),
     solutionPart1,
     solutionPart2,
   )
 where
+import Debug.Trace (trace)
 
-type Input = [String]
+-- * Types
 
-solutionPart1 :: Input -> String
-solutionPart1 = const ("In Progress" :: String)
+type Input = [[Char]]
+
+data Space = Empty | Galaxy
+  deriving (Show, Eq)
+
+data Direction = PlusX | PlusY
+  deriving (Show, Eq)
+
+type AxisVal = Int
+
+type Coord = (AxisVal, AxisVal)
+
+type Range = (AxisVal, AxisVal)
+
+data Line = Line Coord Direction
+  deriving (Show)
+
+type Galaxies = [Coord]
+
+data GalaxyMap = GalaxyMap Range Range Galaxies
+  deriving (Show)
+
+-- * Parsing
+
+parseLine :: AxisVal -> [Char] -> Galaxies
+parseLine y cs = foldl foldCharXCoord [] char'x'zip
+  where
+    char'x'zip = zip cs [0 ..]
+    foldCharXCoord acc (c', x') = case c' of
+      '#' -> (x', y) : acc
+      _ -> acc
+
+prepareInput :: Input -> GalaxyMap
+prepareInput i = GalaxyMap (0,x'max) (0,y'max) galaxies
+  where
+    x'max = (maximum . map length $ i) - 1
+    y'max = length i - 1
+    line'y'zip = zip (reverse i) [0..]
+    foldLineYCoord acc (l',y') = acc <> parseLine y' l'
+    galaxies = foldl foldLineYCoord [] line'y'zip
+
+-- * Solutions
+
+isEmpty :: Line -> Galaxies -> Bool
+isEmpty (Line (_,y) PlusX) = not . any (\(_,y'g) -> y'g == y)
+isEmpty (Line (x,_) PlusY) = not . any (\(x'g,_) -> x'g == x)
+
+fdEmptyYLines :: GalaxyMap -> [Line]
+fdEmptyYLines (GalaxyMap (x'min,x'max) _ gs) = foldl accumEmptyXLines [] [x'min..x'max]
+  where
+    accumEmptyXLines ls x =
+      let l = Line (x,0) PlusY
+      in if isEmpty l gs
+        then l:ls
+        else ls
+
+fdEmptyXLines :: GalaxyMap -> [Line]
+fdEmptyXLines (GalaxyMap _ (y'min,y'max) gs) = foldl accumEmptyYLines [] [y'min..y'max]
+  where
+    accumEmptyYLines ls y =
+      let l = Line (0,y) PlusX
+      in if isEmpty l gs
+        then l:ls
+        else ls
+
+fdEmptyLines :: GalaxyMap -> [Line]
+fdEmptyLines = (<>) <$> fdEmptyXLines <*> fdEmptyYLines
+
+expandEmptyLines :: GalaxyMap -> [Line] -> GalaxyMap
+expandEmptyLines = undefined
+
+expandGalaxy :: GalaxyMap -> GalaxyMap
+expandGalaxy = expandEmptyLines <*> fdEmptyLines
+
+solutionPart1 :: Input -> [Line]
+solutionPart1 = fdEmptyLines . prepareInput
 
 solutionPart2 :: Input -> String
-solutionPart2 = const ("In Progress" :: String) 
+solutionPart2 = const ("In Progress" :: String)
 
 solveDay11 :: FilePath -> IO ()
 solveDay11 input'path = do
