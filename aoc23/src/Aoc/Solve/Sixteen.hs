67de0d6e4580c1ff6aff1bbddd8f2fc659ae4396
handdara <handdara.core@gmail.com>
2023-12-23 01:28:44 -0500

    day 16 solution

--- aoc23/src/Aoc/Solve/Sixteen.hs
+++ aoc23/src/Aoc/Solve/Sixteen.hs
@@ -1,16 +1,31 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# LANGUAGE TupleSections #-}
 
 module Aoc.Solve.Sixteen
   ( solveDay16,
     -- dbg
     Input,
+    BeamFront (..),
+    Direction,
+    Coord,
+    Tile (..),
+    MirrorMchn,
+    north,
+    south,
+    east,
+    west,
+    updateBeamFront,
     solutionPart1,
     solutionPart2,
   )
 where
 
+-- import Data.Group
+
+import Control.Parallel.Strategies
+import Data.Array.IArray ((!))
 import qualified Data.Array.IArray as A
-import Data.Monoid (Sum)
+import qualified Data.Set as S
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
 
 -- * Types
 
@@ -22,23 +37,30 @@ data Tile = Empty | VSplit | HSplit | UMirror | DMirror
 instance Show Tile where
   show = return . tileToChar
 
-type Coord = (Sum Int, Sum Int)
+newtype Coord = Coord (Int, Int) deriving (Show, Eq, Ord, A.Ix)
+
+instance Semigroup Coord where
+  Coord (xl, yl) <> Coord (xr, yr) = Coord (xl + xr, yl + yr)
+
+instance Monoid Coord where
+  mempty = Coord (0, 0)
 
 type Direction = Coord
 
 east :: Direction
-east = (0, 1)
+east = Coord (0, 1)
 
 north :: Direction
-north = (1, 0)
+north = Coord (-1, 0)
 
 west :: Direction
-west = (0, -1)
+west = Coord (0, -1)
 
 south :: Direction
-south = (-1, 0)
+south = Coord (1, 0)
 
-data BeamTip = BeamTip Direction Coord
+data BeamFront = BeamFront Direction Coord
+  deriving (Show, Eq, Ord)
 
 type MirrorMchn = A.Array Coord Tile
 
@@ -60,30 +82,77 @@ charToTile '\\' = DMirror
 charToTile c = error $ "charToTile: unexpected char: " <> [c]
 
 prepareInput :: Input -> MirrorMchn
-prepareInput = undefined
+prepareInput [] = error "prepareInput: called with empty input"
+prepareInput i = A.listArray (Coord (0, 0), maxIdx) (map charToTile . mconcat $ i)
+  where
+    maxIdx = Coord (-1, -1) <> Coord ((,) <$> length <*> length . head $ i)
 
 -- * Solution
 
-updateBeamTip :: Tile -> BeamTip -> [BeamTip]
-updateBeamTip VSplit (BeamTip d c) | d == east || d == west = [BeamTip north (c <> north), BeamTip south (c<>south)]
-updateBeamTip HSplit (BeamTip d c) | d == north || d == south = [BeamTip east (c <> east), BeamTip west (c<>west)]
-updateBeamTip UMirror (BeamTip d c) 
-  | d == east = [BeamTip north (c <> north)]
-  | d == north = [BeamTip east (c <> east)]
-  | d == west = [BeamTip south (c <> south)]
-  | d == south = [BeamTip west (c <> west)]
-updateBeamTip DMirror (BeamTip d c) 
-  | d == east = [BeamTip south (c <> south)]
-  | d == north = [BeamTip west (c <> west)]
-  | d == west = [BeamTip north (c <> north)]
-  | d == south = [BeamTip east (c <> east)]
-updateBeamTip _ (BeamTip d c) = [BeamTip d (c <> d)]
+updateBeamFront :: Tile -> BeamFront -> [BeamFront]
+updateBeamFront VSplit (BeamFront d c) | d == east || d == west = [BeamFront north (c <> north), BeamFront south (c <> south)]
+updateBeamFront HSplit (BeamFront d c) | d == north || d == south = [BeamFront east (c <> east), BeamFront west (c <> west)]
+updateBeamFront UMirror (BeamFront d c)
+  | d == east = [BeamFront north (c <> north)]
+  | d == north = [BeamFront east (c <> east)]
+  | d == west = [BeamFront south (c <> south)]
+  | d == south = [BeamFront west (c <> west)]
+updateBeamFront DMirror (BeamFront d c)
+  | d == east = [BeamFront south (c <> south)]
+  | d == north = [BeamFront west (c <> west)]
+  | d == west = [BeamFront north (c <> north)]
+  | d == south = [BeamFront east (c <> east)]
+updateBeamFront _ (BeamFront d c) = [BeamFront d (c <> d)]
 
-solutionPart1 :: Input -> String
-solutionPart1 = const ("In Progress" :: String)
+inBoundsOf :: Coord -> MirrorMchn -> Bool
+inBoundsOf (Coord (cx, cy)) mm = lbi <= cx && cx <= ubi && lbj <= cy && cy <= ubj
+  where
+    (Coord (lbi, lbj), Coord (ubi, ubj)) = A.bounds mm
 
-solutionPart2 :: Input -> String
-solutionPart2 = const ("In Progress" :: String)
+stepBeams :: MirrorMchn -> ([BeamFront], S.Set BeamFront) -> ([BeamFront], S.Set BeamFront)
+stepBeams mm (bfs, visited) =
+  let bfs'valid = [b | b@(BeamFront _ c) <- bfs, c `inBoundsOf` mm, b `S.notMember` visited]
+      bfs'coords = map (\(BeamFront _ c) -> c) bfs'valid
+      tiles'valid = map (mm !) bfs'coords
+      bfs'new = zip tiles'valid bfs'valid >>= uncurry updateBeamFront
+      visited'new = visited `S.union` S.fromList bfs'valid
+   in (bfs'new, visited'new)
+
+-- | taking a mirror machine configuration and initial beam front, propagate it through the
+-- mirror machine and find the set of all lit coordinates
+propagateBeam :: MirrorMchn -> BeamFront -> S.Set Coord
+propagateBeam mm bf =
+  let x0 = ([bf], S.empty)
+      xf = until (null . fst) (stepBeams mm) x0
+   in S.map (\(BeamFront _ c) -> c) (snd xf)
+
+solutionPart1 :: Input -> Int
+solutionPart1 = S.size . (`propagateBeam` bf'init) . prepareInput
+  where
+    bf'init = BeamFront east mempty
+
+mkInitBeams :: MirrorMchn -> [BeamFront]
+mkInitBeams mm =
+  [BeamFront south c | c <- northBorder]
+    <> [BeamFront north c | c <- southBorder]
+    <> [BeamFront west c | c <- eastBorder]
+    <> [BeamFront east c | c <- westBorder]
+  where
+    (Coord (lbi, lbj), Coord (ubi, ubj)) = A.bounds mm
+    is = [lbi .. ubi]
+    js = [lbj .. ubj]
+    northBorder = map (Coord . (lbi,)) js
+    southBorder = map (Coord . (ubi,)) js
+    westBorder = map (Coord . (,lbj)) is
+    eastBorder = map (Coord . (,ubj)) is
+
+solutionPart2 :: Input -> Int
+solutionPart2 i = maximum litTiles
+  where
+    mm = prepareInput i
+    litTiles =
+      map (S.size . propagateBeam mm) (mkInitBeams mm)
+        `using` parList rseq
 
 solveDay16 :: FilePath -> IO ()
 solveDay16 input'path = do
@@ -91,9 +160,11 @@ solveDay16 input'path = do
 
   let input'lines = lines input'string
 
-  putStrLn "Input: "
-  mapM_ putStrLn input'lines
-
   putStrLn $ "Solution to part 1: " <> show (solutionPart1 input'lines)
 
+  t1 <- getCurrentTime
   putStrLn $ "Solution to part 2: " <> show (solutionPart2 input'lines)
+  t2 <- getCurrentTime
+
+  let durPart1 = diffUTCTime t2 t1
+  putStrLn $ "Part 2 Solution time: " <> show durPart1
